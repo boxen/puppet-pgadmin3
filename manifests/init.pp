0105233df7182cdf4c58629aa96a978fb4593ef4
Salimane Adjao Moustapha <me@salimane.com>
2014-12-06 00:31:24 +0100

    add version parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,9 @@
 # Examples
 #
 #   include pgadmin3
-class pgadmin3 {
+class pgadmin3($version = '1.18.1') {
   package { 'pgAdmin3':
     provider => 'appdmg_eula',
-    source   => 'http://ftp.postgresql.org/pub/pgadmin3/release/v1.18.1/osx/pgadmin3-1.18.1.dmg',
+    source   => "http://ftp.postgresql.org/pub/pgadmin3/release/v${version}/osx/pgadmin3-${version}.dmg",
   }
 }
